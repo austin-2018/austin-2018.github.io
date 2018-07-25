75a85fe52037676c81c80c598ee7398646ebe4b6
austin-2018 <38106983+austin-2018@users.noreply.github.com>
2018-07-25 14:21:54 -0400

    Update geolocate.js

--- js/geolocate.js
+++ js/geolocate.js
@@ -1,3 +1,9 @@
+var c = function(pos) {
+  var lat     = pos.coords.latitude,
+      long    = pos.coords.longitude,
+      coords  = lat + '' + ',' + '' + long;
+}
+
 document.getElementById('get_location').onclick = function {
   navigator.geolocation.getCurrentPosition(c);
   return false;
