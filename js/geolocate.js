2a2c7ecf50c1cfa1b3de6c13760339813beeec4c
austin-2018 <38106983+austin-2018@users.noreply.github.com>
2018-07-25 16:14:32 -0400

    Update geolocate.js

--- js/geolocate.js
+++ js/geolocate.js
@@ -1,12 +1,12 @@
 var c = function(pos) {
   var lat     = pos.coords.latitude,
       long    = pos.coords.longitude,
-      coords  = lat + '' + ',' + '' + long;
-  
+      coords  = lat + ',' + long;
+
   document.getElementById('google_map').setAttributte('src', 'https://maps.google.co.uk/?q=' + coords + '&z=60&output=embed');
 }
 
-document.getElementById('get_location').onclick = function() {
+  document.getElementById('get_location').onclick = function() {
   navigator.geolocation.getCurrentPosition(c);
   return false;
 }
