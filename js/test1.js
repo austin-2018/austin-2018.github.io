0dfd6db54962a3a7854589c625aa958efec28071
austin-2018 <38106983+austin-2018@users.noreply.github.com>
2018-07-28 19:52:27 -0400

    Update test1.js

--- js/test1.js
+++ js/test1.js
@@ -15,4 +15,15 @@ request.onload = function () {
   }
 }
 
-request.send();     
+request.send();
+
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
