9b67a092a63bf2c0167ad1648ddff0c16f2e23c3
austin-2018 <38106983+austin-2018@users.noreply.github.com>
2018-08-07 11:42:56 -0400

    Create fb1-version1.js

---
+++ js-versions/fb1-version1.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD123PvPBnD0L4Nw6DKvlyjPzYIE9Sx_Jo",
+    authDomain: "find-me-eba22.firebaseapp.com",
+    databaseURL: "https://find-me-eba22.firebaseio.com",
+    projectId: "find-me-eba22",
+    storageBucket: "find-me-eba22.appspot.com",
+    messagingSenderId: "511040371879"
+  };
+  firebase.initializeApp(config);
