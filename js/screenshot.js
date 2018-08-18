d6958648adab96dab9a6def8f00aa65ce8970c1e
austin-2018 <38106983+austin-2018@users.noreply.github.com>
2018-08-18 16:46:53 -0400

    Update screenshot.js

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,100 +1,99 @@
-    
-                        const hdConstraints = {
-                          video: {width: {min: 1280}, height: {min: 720}}
-                        };
-                        navigator.mediaDevices.getUserMedia(hdConstraints).
-                          then((stream) => {video.srcObject = stream});
+const hdConstraints = {
+  video: {width: {min: 1280}, height: {min: 720}}
+};
+navigator.mediaDevices.getUserMedia(hdConstraints).
+  then((stream) => {video.srcObject = stream});
 
-                       const vgaConstraints = {
-                          video: {width: {exact: 640}, height: {exact: 480}}
-                        };
-                        navigator.mediaDevices.getUserMedia(vgaConstraints).
-                          then((stream) => {video.srcObject = stream}); 
+const vgaConstraints = {
+  video: {width: {exact: 640}, height: {exact: 480}}
+};
+navigator.mediaDevices.getUserMedia(vgaConstraints).
+  then((stream) => {video.srcObject = stream}); 
 
-                      const captureVideoButton =
-                       document.querySelector('#screenshot .capture-button');
-                      const screenshotButton = document.querySelector('#screenshot-button');
-                      const img = document.querySelector('#screenshot img');
-                      const video = document.querySelector('#screenshot video');
-                      const canvas = document.createElement('canvas');
+const captureVideoButton =
+document.querySelector('#screenshot .capture-button');
+const screenshotButton = document.querySelector('#screenshot-button');
+const img = document.querySelector('#screenshot img');
+const video = document.querySelector('#screenshot video');
+const canvas = document.createElement('canvas');
 
-                     captureVideoButton.onclick = function() {
-                       navigator.mediaDevices.getUserMedia(constraints).
-                         then(handleSuccess).catch(handleError);
-                     };
+captureVideoButton.onclick = function() {
+navigator.mediaDevices.getUserMedia(constraints).
+ then(handleSuccess).catch(handleError);
+};
 
-                     screenshotButton.onclick = video.onclick = function() {
-                       canvas.width = video.videoWidth;
-                       canvas.height = video.videoHeight;
-                       canvas.getContext('2d').drawImage(video, 0, 0);
-                       // Other browsers will fall back to image/png
-                       img.src = canvas.toDataURL('image/webp');
-                       imageData = img.src; 
-                       //img.src = canvas.toDataURL('image/png');
-                       //https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
-                       document.getElementById("hidden-screenshot").value = canvas.toDataURL('image/webp');                   
-                       document.getElementsByName("myHiddenField")[0].setAttribute("value", imageData);
-                     };
+screenshotButton.onclick = video.onclick = function() {
+canvas.width = video.videoWidth;
+canvas.height = video.videoHeight;
+canvas.getContext('2d').drawImage(video, 0, 0);
+// Other browsers will fall back to image/png
+img.src = canvas.toDataURL('image/webp');
+imageData = img.src; 
+//img.src = canvas.toDataURL('image/png');
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+document.getElementById("hidden-screenshot").value = canvas.toDataURL('image/webp');                   
+document.getElementsByName("myHiddenField")[0].setAttribute("value", imageData);
+};
 
-                     function handleSuccess(stream) {
-                       screenshotButton.disabled = false;
-                       video.srcObject = stream;
-                     }
-                 
-                    const videoElement = document.querySelector('video');
-                           const audioSelect = document.querySelector('select#audioSource');
-                           const videoSelect = document.querySelector('select#videoSource');
+function handleSuccess(stream) {
+screenshotButton.disabled = false;
+video.srcObject = stream;
+}
 
-                           navigator.mediaDevices.enumerateDevices()
-                             .then(gotDevices).then(getStream).catch(handleError);
+const videoElement = document.querySelector('video');
+   const audioSelect = document.querySelector('select#audioSource');
+   const videoSelect = document.querySelector('select#videoSource');
 
-                           audioSelect.onchange = getStream;
-                           videoSelect.onchange = getStream;
+   navigator.mediaDevices.enumerateDevices()
+     .then(gotDevices).then(getStream).catch(handleError);
 
-                           function gotDevices(deviceInfos) {
-                             for (let i = 0; i !== deviceInfos.length; ++i) {
-                               const deviceInfo = deviceInfos[i];
-                               const option = document.createElement('option');
-                               option.value = deviceInfo.deviceId;
-                               if (deviceInfo.kind === 'audioinput') {
-                                 option.text = deviceInfo.label ||
-                                   'microphone ' + (audioSelect.length + 1);
-                                 audioSelect.appendChild(option);
-                               } else if (deviceInfo.kind === 'videoinput') {
-                                 option.text = deviceInfo.label || 'camera ' +
-                                   (videoSelect.length + 1);
-                                 videoSelect.appendChild(option);
-                               } else {
-                                 console.log('Found another kind of device: ', deviceInfo);
-                               }
-                             }
-                           }
+   audioSelect.onchange = getStream;
+   videoSelect.onchange = getStream;
 
-                        function getStream() {
-                          if (window.stream) {
-                            window.stream.getTracks().forEach(function(track) {
-                              track.stop();
-                            });
-                          }
+   function gotDevices(deviceInfos) {
+     for (let i = 0; i !== deviceInfos.length; ++i) {
+       const deviceInfo = deviceInfos[i];
+       const option = document.createElement('option');
+       option.value = deviceInfo.deviceId;
+       if (deviceInfo.kind === 'audioinput') {
+         option.text = deviceInfo.label ||
+           'microphone ' + (audioSelect.length + 1);
+         audioSelect.appendChild(option);
+       } else if (deviceInfo.kind === 'videoinput') {
+         option.text = deviceInfo.label || 'camera ' +
+           (videoSelect.length + 1);
+         videoSelect.appendChild(option);
+       } else {
+         console.log('Found another kind of device: ', deviceInfo);
+       }
+     }
+   }
 
-                          const constraints = {
-                            audio: {
-                              deviceId: {exact: audioSelect.value}
-                            },
-                            video: {
-                              deviceId: {exact: videoSelect.value}
-                            }
-                          };
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
 
-                          navigator.mediaDevices.getUserMedia(constraints).
-                            then(gotStream).catch(handleError);
-                        }
+  const constraints = {
+    audio: {
+      deviceId: {exact: audioSelect.value}
+    },
+    video: {
+      deviceId: {exact: videoSelect.value}
+    }
+  };
 
-                        function gotStream(stream) {
-                          window.stream = stream; // make stream available to console
-                          videoElement.srcObject = stream;
-                        }
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream).catch(handleError);
+}
 
-                        function handleError(error) {
-                          console.error('Error: ', error);
-                        }
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+}
+
+function handleError(error) {
+  console.error('Error: ', error);
+}
